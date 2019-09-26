383ab097adcb51be0f81023c7100ea20b25a0156
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-09-26 09:18:07 +0200

    Ejercicio 1

---
+++ 1.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Prueba de PHP</title>
+</head>
+<body>
+	<?php 
+	
+	$arraia = array();
+	$faktoriala = array();
+	$faktorea= 1;
+	$faktoriala[0]=0;
+	for($i=0;$i<11;$i++){
+		$arraia[$i]=$i;
+		echo $arraia[$i];
+		if($i>0){
+			$faktorea=$faktorea*$i;
+			$faktoriala[$i]=$faktorea;
+			
+		}
+		echo "&nbsp; ".$faktoriala[$i]."<br>";
+
+
+	}
+
+
+	
+
+	?>
+</body>
+</html>
