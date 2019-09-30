9480c6c2353c39da36eeb90db5e7b4e53d2bc1e2
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-09-30 13:32:07 +0200

    Ejercicio 8

---
+++ 8.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Prueba de PHP</title>
+</head>
+<body>
+	<?php
+	function exist ($file){
+			try {
+			if (!file_exists($file))
+				throw new exception("Fitxategia ez da aurkitu");
+						
+		} catch (Error $e) {
+			echo $e->getMessage(), "\n";
+		}
+	}
+	exist("39.php");
+?>
+</body>
+</html>
